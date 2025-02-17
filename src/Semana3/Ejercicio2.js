61f6247589a66b4ea5c4fe5e32814a985baad113
wandaglezest <wanda.glez.est@gmail.com>
2025-02-17 13:05:46 +0100

    13:05

---
+++ src/Semana3/Ejercicio2.js
@@ -0,0 +1,9 @@
+/ Creando las variables
+let nombre = "Juan";
+let edad = 30;
+let ciudad = "Madrid";
+
+// Mostrando las variables en la consola
+console.log("Nombre: " + nombre);
+console.log("Edad: " + edad);
+console.log("Ciudad: " + ciudad);
